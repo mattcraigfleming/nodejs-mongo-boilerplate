72dc94e0aa5e9e49245905876e37b1f7b872ba5d
mattcraigfleming <mfleming2889@gmail.com>
2020-06-03 03:07:28 +0100

    latest changes

--- README.md
+++ README.md
@@ -9,3 +9,17 @@ MONGO_PASS=
 JWT_SECRET=
 
 ```
+
+```sh
+api/
+├── migrations     # Migrations for seeding the database with some initial data
+├── models         # Handle talking to the database
+├── mutations      # Mutation     resolvers
+├── queries        # Query        resolvers
+├── subscriptions  # Subscription resolvers
+├── types          # The schema, split up into many smaller parts
+│   └── scalars.js # The custom scalars we use in our schema and their resolvers
+├── README.md
+├── index.js       # Runs the actual servers (GraphQL + WebSocket for subscriptions)
+└── schema.js      # Combines the types from types/ and the resolvers together with graphql-tools
+```
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
+import rateLimit from 'express-rate-limit'
 import morgan from 'morgan'
 import apiRoutes from './routes/api'
 
@@ -11,13 +12,20 @@ mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true })
 
 const connection = mongoose.connection
 
-connection.once('open', function () {
+connection.once('open', () =>
   console.log('MongoDB database connection established successfully')
-})
+)
 const app = express()
 const port = 3000
 
 app.use(morgan('dev'))
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+})
+
+app.use(limiter)
 
 app.use(bodyParser.json())
 
